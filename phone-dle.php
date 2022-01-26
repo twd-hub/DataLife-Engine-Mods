b6f7873d550b0c5afbae992b3f96b7a85e53a9ae
TWD SoftWare <95255933+twdsoft@users.noreply.github.com>
2022-01-26 15:32:49 +0300

    Пример и ссылки в файле
    Модуль добавляет номера телефонов при регистрации - DLE

---
+++ phone-dle.php
@@ -0,0 +1,28 @@
+<html><head>Not points script</head><body>
+
+<p><h3>Для наглядности все размещено внутри данного файла!<h3></p>
+<p><strong>В разработке затронуты файлы:</strong></p>
+<code>\engine\modules\register.php 
+\engine\modules\profile.php
+\engine\modules\editprofile.php
+\language\Russian\adminpanel.lng
+\language\Russian\website.lng
+\engine\ajax\adminfunction.php
+\engine\ajax\editprofile.php
+\engine\inc\include\functions.inc.php
+\engine\inc\options.php
+\engine\inc\plugins.php
+\engine\inc\preview.php
+\engine\inc\usergroup.php</code>
+<p><pДобавлено необходимое количество столбцов в БД</p>
+<p></p>
+<p>Установлена Jquery заглушка для ввода только цифр, в определенном количестве.</p>
+<p>Добавлен выпадающий список с выбором страны и оператора.</p>
+<p>Отображение в Профиле: Видят только Администраторы и сам пользователь (По желанию можно сделать видимым)</p>
+<p>Настройка в Админпанели - на случай, если хозяин решит менят или добавлять данные только по заявкам, отключив опцию редактуры пользователем.</p>
+<p><strong>Стоимость модуля:</strong></p>
+<p>- 1 номер телефона - 10 USD</p>
+<p>- 2 номера - 20 USD и т.д.</p>
+<p>Донастройка профиля и админки - 10 USD</p>
+<a href="https://vk.com/twd.software">Заказать и купить здесь</a>
+</body></html>
